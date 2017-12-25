426d86fd641b84e3851d7ea0e3ae906cff9aacfa
Alex Timofeev <atimofeev@reactant.ru>
2017-12-25 11:08:04 +0300

    Use updated version of Middleman

--- Gemfile
+++ Gemfile
@@ -1,12 +1,7 @@
-source 'http://rubygems.org'
-ruby '2.3.1'
+source 'https://rubygems.org'
 
-gem 'middleman', '~>3.4.1'
-gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
-gem 'slim'
-gem 'jquery-middleman'
-
-gem 'rack-contrib'
-gem 'puma'
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
 gem 'foreman'
-gem 'rake'
--- config.rb
+++ config.rb
@@ -1,87 +1,46 @@
-###
-# Compass
-###
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-# Change Compass configuration
-compass_config do |config|
-  config.output_style = :compact
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
 end
 
-###
-# Page options, layouts, aliases and proxies
-###
+# Layouts
+# https://middlemanapp.com/basics/layouts/
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+# page '/path/to/file.html', layout: 'other_layout'
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
 
-###
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
 # Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-#activate :livereload
-
 # Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
-
-set :full_name, "Alex Timofeev"
-set :position_title, "Full-stack Ruby developer, Front-end Architect"
-set :og_description, "For my next challenge, I'm interested in a full-time remote job as a Ruby (on and off Rails) Engineer with involvement in front-end development."
-set :github_handle, "query-string"
-set :twitter_handle, "query_string"
-set :linkedin_handle, "query-string"
-set :email, "alex@query-string.com"
-
-configure :development do
-  set :host, "http://localhost:4567"
-end
-
-configure :build do
-  set :host, "http://www.query-string.com/"
-end
-
-activate :i18n, langs: [:en]
-
 # Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  activate :minify_css
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-  # Minify Javascript on build
-  activate :minify_javascript
-
-  # Enable cache buster
-  activate :asset_hash
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-end
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
